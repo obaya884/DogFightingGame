7a65868be2842a8eaee2524339480e54bbda4f64
obaya884 <taku3.884@gmail.com>
2022-04-29 16:23:07 +0900

    プレイヤー移動の実装

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    Rigidbody rigidbody;
+
+    float x;
+    float z;
+    float moveSpeed = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // キーボード入力で移動
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        // 速度設定
+        rigidbody.velocity = new Vector3(x, 0, z) * moveSpeed;
+
     }
 }
